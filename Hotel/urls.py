617f70608dff25152cbf14f0e25f357bd871d0e0
abbas <abi.esmaeili@gmail.com>
2023-02-24 20:19:20 +0330

    third

--- Hotel/urls.py
+++ Hotel/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'hotel'
 urlpatterns=[
     path('',views.HomeView.as_view(),name='home'),
-    path('detail/<int:id>',views.DetailView.as_view(),name='detail'),
-    path('reserved/<int:room_id>',views.ReservedView.as_view(),name='reserved'),
+    # path('detail/<int:id>',views.DetailView.as_view(),name='detail'),
+    path('reserved/<int:room_number>',views.ReservedView.as_view(),name='reserved'),
 
 ]
--- Hotel/views.py
+++ Hotel/views.py
@@ -12,32 +12,37 @@ class HomeView(View):
          return render(request,'index.html',{"rooms":rooms,'form':form})
 
 
-class DetailView(View):
-    def post(self,request,room_id):
-        room = Room.objects.get(id == room_id)
-        form = AddForm()
+# class DetailView(View):
+#     def post(self,request,room_id):
+#         room = Room.objects.get(id == room_id)
+#         form = AddForm()
 
-        if room.is_availble == True :
-            form.save()
-            return redirect('hotel:reserved',{'form':form})
+#         if room.is_availble == True :
+#             form.save()
+#             return redirect('hotel:reserved',{'form':form})
 
 
 
 class ReservedView(View):
     form_class = AddForm
-    def dispatch(self, request, *args, **kwargs):
-        room = Room.objects.get(pk = kwargs['room_id'])
-        room.is_availble = False
-        return render(request,'reserved.html',{'room':room})
+    # def dispatch(self, request, *args, **kwargs):
+    #     super().dispatch(request, *args, **kwargs)
 
-        return super().dispatch(request, *args, **kwargs)
+    #     room = Room.objects.get(pk = kwargs['id'])
+    #     # room.is_availble = False
+    #     return render(request,'reserved.html',{'room':room})
 
-    def get(self,request,room_id):
-        form =self.form_class
-        return render(request,'reserved.html',{'form':form})
 
-    def post(self, request, room_id):
-        room = Room.objects.get(number = room_id)
+    # def get(self,request,id):
+    #     form =self.form_class
+    #     return render(request,'reserved.html')
+
+    def post(self, request, room_number):
+        print('***********')
+        print("hello")
+        print('***********')
+
+        room = Room.objects.get(number = room_number)
         form = self.form_class(request.POST,instance=room)
         # print('***********')
         # print(room)
